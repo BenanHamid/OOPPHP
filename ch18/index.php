866e02b68d17b4166c914648d6f8ef550fc871f4
Benan <benan.b.hamid@gmail.com>
2014-07-10 12:04:55 +0300

    More tweeking

--- ch18/index.php
+++ ch18/index.php
@@ -1,4 +1,11 @@
 <?php
 include 'registry.php';
 
-$a = new Registry();
+Registry::setData('name', 'benan');
+$a = Registry::getData('name');
+echo $a.'<br>';
+
+Registry::setData('sss', 'asdasdas');
+$b = Registry::getData('sss');
+echo $b.'<br>';
+echo $a.'<br>';
--- ch18/registry.php
+++ ch18/registry.php
@@ -1,5 +1,15 @@
 <?php
 
 class Registry{
+	private static $data = null;
 	
+	private function __construct(){
+	
+	}
+	public static function setData($key, $value){
+		self::$data[$key] = $value;
+	}
+	public static function getData($key){
+		return self::$data[$key];
+	}
 }
