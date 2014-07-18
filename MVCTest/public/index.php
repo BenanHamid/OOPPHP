2f73db268c1353faabea20c1301a282a0271e160
Benan <benan.b.hamid@gmail.com>
2014-07-18 12:01:46 +0300

    MVC Start

---
+++ MVCTest/public/index.php
@@ -0,0 +1,5 @@
+<?php
+include '../../MVC/App.php';
+//VIMP на нетбийнс пропъртиес на папката инклуде файл пат и там намирам папката на фреймвоурка
+$app = \myFramework\App::getInstance();
+$app->run();
